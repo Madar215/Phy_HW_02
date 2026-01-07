0c80f159cc3b050eb0efe7247df383c8c601c534
Madar215 <galmadar2424@gmail.com>
2026-01-07 11:46:18 +0200

    Basic Movement

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class CarController : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class CarController : MonoBehaviour {
+    [SerializeField] private InputReader inputReader;
+    [SerializeField] private Rigidbody rb;
+
+    [Header("Forces")] 
+    [SerializeField] private float moveSpeed = 10f;
+
+    private Vector2 _move;
+    private float _moveX;
+    private float _moveY;
+
+    private void OnEnable() {
+        inputReader.Move += OnMove;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnDisable() {
+        inputReader.Move -= OnMove;
+    }
+
+    private void FixedUpdate() {
+        var movement = new Vector3(_moveX, 0f, _moveY) * (moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement);
+    }
+
+    private void OnMove(InputAction.CallbackContext context) {
+        _move = context.ReadValue<Vector2>();
+        _moveX = _move.x;
+        _moveY = _move.y;
     }
 }
---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(fileName = "InputReader", menuName = "Scriptable Objects/InputReader")]
+public class InputReader : ScriptableObject, PlayerInputActions.IPlayerActions {
+    // Events
+    public event UnityAction<InputAction.CallbackContext> Move = delegate { };
+    
+    private PlayerInputActions _inputActions;
+    
+    private void OnEnable() {
+        if (_inputActions == null) {
+            _inputActions = new PlayerInputActions();
+            _inputActions.Player.SetCallbacks(this);
+        }
+        
+        _inputActions.Player.Enable();
+    }
+
+    private void OnDisable() {
+        _inputActions.Player.Disable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context) {
+        Move?.Invoke(context);
+    }
+
+    public void OnLook(InputAction.CallbackContext context) {
+        
+    }
+
+    public void OnAttack(InputAction.CallbackContext context) {
+        
+    }
+
+    public void OnInteract(InputAction.CallbackContext context) {
+        
+    }
+
+    public void OnCrouch(InputAction.CallbackContext context) {
+        
+    }
+
+    public void OnJump(InputAction.CallbackContext context) {
+        
+    }
+
+    public void OnPrevious(InputAction.CallbackContext context) {
+        
+    }
+
+    public void OnNext(InputAction.CallbackContext context) {
+        
+    }
+
+    public void OnSprint(InputAction.CallbackContext context) {
+        
+    }
+}
